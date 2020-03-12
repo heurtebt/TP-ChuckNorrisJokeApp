eee481f5a1b9ce8571eef7ac2f1b21b909ae9a63
heurtebt <theo.heurtebize@edu.esiee.fr>
2020-03-12 09:00:08 +0100

    ok

--- app/src/main/java/com/example/chucknorrisjokeapp/MainActivity.kt
+++ app/src/main/java/com/example/chucknorrisjokeapp/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
