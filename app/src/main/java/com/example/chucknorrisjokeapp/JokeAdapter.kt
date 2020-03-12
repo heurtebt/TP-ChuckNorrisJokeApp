b6b7af8e66a99e5fe7abb02337fade50138b9341
heurtebt <theo.heurtebize@edu.esiee.fr>
2020-03-12 10:55:11 +0100

    Ajout de joke_layout et modification de JokeAdapter en conséquence

--- app/src/main/java/com/example/chucknorrisjokeapp/JokeAdapter.kt
+++ app/src/main/java/com/example/chucknorrisjokeapp/JokeAdapter.kt
@@ -1,19 +1,23 @@
 package com.example.chucknorrisjokeapp
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.joke_layout.view.*
 
 class JokeAdapter(private val jokes: List<String>) :
 RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
 
-    class JokeViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class JokeViewHolder(val jokeView: LinearLayout) : RecyclerView.ViewHolder(jokeView)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeAdapter.JokeViewHolder {
-        val textView =  TextView(parent.context)
-        return JokeViewHolder(textView)
+        val jokeView =   LayoutInflater.from(parent.context)
+            .inflate(R.layout.joke_layout, parent, false) as LinearLayout
+        return JokeViewHolder(jokeView)
     }
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
-        holder.textView.text = jokes[position]
+        holder.jokeView.textView.text = jokes[position]
     }
     override fun getItemCount() = jokes.size
 }
