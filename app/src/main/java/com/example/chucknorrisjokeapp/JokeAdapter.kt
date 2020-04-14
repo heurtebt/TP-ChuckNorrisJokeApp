c28c068d1d16205086b6e54c644a0dab9a80bfe9
heurtebt <theo.heurtebize@edu.esiee.fr>
2020-04-14 11:59:49 +0200

    Affichage d'une Single Joke Abandon de la liste de String initiale Modification du JokeAdapter.kt par l'ajout d'une MutableList de Joke et ajout de fonctions pour ajouter des objets Joke dans la MutableList Ajout de AndroidSchedulers.mainThread() dans observeOn() et d'un bouton Floating Action Button pour l'ajout de nouvelle Joke

--- app/src/main/java/com/example/chucknorrisjokeapp/JokeAdapter.kt
+++ app/src/main/java/com/example/chucknorrisjokeapp/JokeAdapter.kt
@@ -7,9 +7,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.joke_layout.view.*
 
-class JokeAdapter(private val jokes: List<String>) :
+class JokeAdapter() :
 RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
-
+    private val jokes : MutableList<Joke> = mutableListOf<Joke>()
     class JokeViewHolder(val jokeView: LinearLayout) : RecyclerView.ViewHolder(jokeView)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeAdapter.JokeViewHolder {
         val jokeView =   LayoutInflater.from(parent.context)
@@ -17,7 +17,16 @@ RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
         return JokeViewHolder(jokeView)
     }
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
-        holder.jokeView.textView.text = jokes[position]
+        holder.jokeView.textView.text = jokes[position].value
     }
     override fun getItemCount() = jokes.size
+
+    fun addJoke(jokeInput : Joke){
+        jokes.add(jokeInput)
+        notifyDataSetChanged()
+    }
+    fun addJokes(jokesInput : MutableList<Joke>){
+        jokes.addAll(jokesInput)
+        notifyDataSetChanged()
+    }
 }
